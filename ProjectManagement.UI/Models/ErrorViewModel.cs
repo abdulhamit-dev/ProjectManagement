2bec64b5ff2bc8164e94db5ff0c03b1f3fc6dfba
Hamit <hmd62392@gmail.com>
2022-04-02 23:12:49 +0300

    Create Business, DataAccess , API , UI

---
+++ ProjectManagement.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectManagement.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
